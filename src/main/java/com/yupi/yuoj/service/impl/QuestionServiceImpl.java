c1841d86ff8c8861c9654666dde951b95615d1b4
fanshuaiyao <1345822123@qq.com>
2024-12-05 18:34:50 +0800

    项目V1.0完结

--- src/main/java/com/yupi/yuoj/service/impl/QuestionServiceImpl.java
+++ src/main/java/com/yupi/yuoj/service/impl/QuestionServiceImpl.java
@@ -154,8 +154,6 @@ public class QuestionServiceImpl extends ServiceImpl<QuestionMapper, Question>
         Map<Long, List<User>> userIdUserListMap = userService.listByIds(userIdSet).stream()
                 .collect(Collectors.groupingBy(User::getId));
 
-
-
         // 填充信息
         List<QuestionVO> questionVOList = questionList.stream().map(question -> {
             QuestionVO questionVO = QuestionVO.objToVo(question);
